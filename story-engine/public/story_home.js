b4414ff55a571736fbff8ddedd8088eb6a71574c
Juss <286642846+jussray@users.noreply.github.com>
2026-07-07 17:17:20 -0400

    Wire predictive OODA link from story workspace home

--- story-engine/public/story_home.js
+++ story-engine/public/story_home.js
@@ -11,6 +11,7 @@ document.getElementById('chaptersLink').href = `/chapters.html${qs}`;
 document.getElementById('movieLink').href = `/movie.html${qs}`;
 document.getElementById('lindymodeLink').href = `/lindymode_dashboard.html${qs}`;
 document.getElementById('decisionLink').href = `/decision_dashboard.html${qs}`;
+document.getElementById('learningLink').href = `/learning_dashboard.html${qs}`;
 document.getElementById('eventsLink').href = `/events_view.html${qs}`;
 
 fetch(`/api/story/${encodeURIComponent(workspace_id)}`)
